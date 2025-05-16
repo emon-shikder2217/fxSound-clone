8baabefe2699b9405ed3bb5c38e716c490c3b677
Emon_Shikder <emonshikder2217@gmail.com>
2025-05-16 23:45:11 +0600

    initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../assets/header-img.png';
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/header-img.png";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Header = () => {
+  const style = {
+    textDecoration: "underline",
+    cursor: "pointer",
+  };
   return (
-    <header className="header">
-      <div className="logo">
-        <img className='logoImg' src={logo} alt="logo" />
+    <>
+      <header className="header">
+        <div className="logo">
+          <img className="logoImg" src={logo} alt="logo" />
+        </div>
+        <nav className="nav">
+          <a href="#download" className="download">
+            Download
+          </a>
+          <a href="#learn" className="learn">
+            Learn{" "}
+            <span className="icon">
+              <MdOutlineKeyboardArrowDown className="icon" />
+            </span>
+          </a>
+          <a href="#contribute">Contribute</a>
+        </nav>
+      </header>
+      <div className="line">
+        <p className="lineTxt">
+          FxSound is now open-source and we're looking for{" "}
+          <span style={style}>contributors</span>
+        </p>
       </div>
-      <nav className="nav">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
-      </nav>
-    </header>
+    </>
   );
 };
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './Hero.css';
-import heroImage from '../../assets/hero-bg.jpg';
-import Header from '../header/Header';
+import React from "react";
+import "./Hero.css";
+import Header from "../header/Header";
+import heroImg from "../../assets/hero-img.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <Header />
+      <div className="hero-container">
+        <div className="hero-content">
+          <h1 className="mainTitle">Get better sound</h1>
+          <p className="subTitle">
+            Free open-source software to boost sound quality, volume, and bass.
+            Including an equalizer, effects, and presets for customized audio.
+          </p>
+          <button className="hero-btn">Download For Windows</button>
+        </div>
+        <div className="hero-img">
+          <img className="heroImage" src={heroImg} alt="HERO" />
+        </div>
+      </div>
     </div>
   );
 };
