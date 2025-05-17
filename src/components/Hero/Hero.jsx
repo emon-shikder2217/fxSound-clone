9d32db3dae58fad9df71c7e030b41682fec1b298
Emon_Shikder <emonshikder2217@gmail.com>
2025-05-17 16:26:15 +0600

    initial commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Hero.css";
-import Header from "../header/Header";
+import Header from "../Header/Header";
 import heroImg from "../../assets/hero-img.png";
 
 const Hero = () => {
