1cd365a0e70526be7a2780cd0860461c6cddf1cf
Emon_Shikder <emonshikder2217@gmail.com>
2025-05-17 16:16:50 +0600

    2nd page complete

--- src/components/Page1/Page1.jsx
+++ src/components/Page1/Page1.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Page1.css";
-import musicIcon from '../../assets/music.png';
-import controllerIcon from '../../assets/controller.png';
+import musicIcon from "../../assets/music.png";
+import controllerIcon from "../../assets/controller.png";
 import micIcon from "../../assets/mic.svg";
 import fileIcon from "../../assets/film.png";
+import circle from "../../assets/circle.svg";
+import circle2 from "../../assets/circle2.svg";
+import circle3 from "../../assets/circle3.svg";
 
 const features = [
   {
     icon: musicIcon,
+    circle: circle,
+    circle2: circle2,
+    circle3: circle3,
     alt: "music",
     className: "child1 child1Icon",
     title: "Music",
@@ -15,6 +21,9 @@ const features = [
   },
   {
     icon: controllerIcon,
+    circle: circle,
+    circle2: circle2,
+    circle3: circle3,
     alt: "controller",
     className: "child2 child2Icon",
     title: "Video Games",
@@ -22,6 +31,9 @@ const features = [
   },
   {
     icon: fileIcon,
+    circle: circle,
+    circle2: circle2,
+    circle3: circle3,
     alt: "tv",
     className: "child3 child3Icon",
     title: "Tv and Movies",
@@ -29,6 +41,9 @@ const features = [
   },
   {
     icon: micIcon,
+    circle: circle,
+    circle2: circle2,
+    circle3: circle3,
     alt: "mic",
     className: "child4 child1Icon",
     title: "Transcription",
@@ -44,16 +59,31 @@ const Page1 = () => {
           <h1>Amazing sound for everything you listen to</h1>
         </div>
         <div className="parent1">
-          {features.map((feature, idx) => (
-            <div className={`child${idx + 1}`} key={idx}>
+          {features.map((feature, index) => (
+            <div className={`child${index + 1}`} key={index}>
               <div className={feature.className}>
+                <img
+                  src={feature.circle}
+                  className={`circle circle${index + 1}-1`}
+                  alt="circle"
+                />
                 <img
                   src={feature.icon}
                   alt={feature.alt}
                   className={feature.alt}
                 />
+                <img
+                  src={feature.circle2}
+                  className={`circle2 circle${index + 1}-2`}
+                  alt="circle2"
+                />
+                <img
+                  src={feature.circle3}
+                  className={`circle3 circle${index + 1}-3`}
+                  alt="circle3"
+                />
               </div>
-              <div className={`child${idx + 1}Txt`}>
+              <div className={`child${index + 1}Txt`}>
                 <h3>{feature.title}</h3>
                 <p>{feature.text}</p>
               </div>
